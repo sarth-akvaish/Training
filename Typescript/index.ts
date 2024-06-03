fcddc4af7ba3f14e673394c24fe621bbb6e838d3
Sarthak <sarthakvkumar@gmail.com>
2024-06-03 09:16:12 +0530

    learning interfaces

--- Typescript/index.ts
+++ Typescript/index.ts
@@ -1,19 +1,42 @@
-type User = {
-    readonly _id: string;
-    name: string;
-    email: string;
-    isACctive: boolean;
-    creaditCardDetails?: number
+// type User = {
+//     readonly _id: string;
+//     name: string;
+//     email: string;
+//     isACctive: boolean;
+//     creaditCardDetails?: number
+// }
+
+// let myUser: User = {
+//     _id: "1242",
+//     name: "dsnksjf",
+//     email: "kdfnsfn",
+//     isACctive: false
+// }
+
+// myUser.email = "sarthak@gmail.com"
+
+// Interfaces 
+
+interface User {
+    readonly db_id: number
+    email: string,
+    userId: number,
+    googleId?: string,
+    startTrail(): string,
+    getCoupon(couponName: string, value: number): number
 }
 
-let myUser: User = {
-    _id: "1242",
-    name: "dsnksjf",
-    email: "kdfnsfn",
-    isACctive: false
+const Hari: User = {
+    db_id: 12, email: "hari@gmail.com", userId: 12313,
+    startTrail: () => {
+        return 'checking'
+    },
+    getCoupon: (name: 'coupon200', value: 20) => {
+        return value
+    }
 }
 
-myUser.email = "sarthak@gmail.com"
 
 
+// difference between interface and type 
 export { }
