061fc334162c2e90ff3e6761f440563fc7b623a1
Sarthak <sarthakvkumar@gmail.com>
2024-06-03 09:29:45 +0530

    type modifiers in Typescript

--- Typescript/index.ts
+++ Typescript/index.ts
@@ -17,34 +17,49 @@
 
 // Interfaces 
 
-interface User {
-    readonly db_id: number
-    email: string,
-    userId: number,
-    googleId?: string,
-    startTrail(): string,
-    getCoupon(couponName: string, value: number): number
-}
+// interface User {
+//     readonly db_id: number
+//     email: string,
+//     userId: number,
+//     googleId?: string,
+//     startTrail(): string,
+//     getCoupon(couponName: string, value: number): number
+// }
 
-interface User {
-    googleToken: string
-}
+// interface User {
+//     googleToken: string
+// }
 
-interface Admin extends User {
-    role: "admin" | "ta" | "user",
-}
-const Hari: Admin = {
-    role: "admin",
-    db_id: 12, email: "hari@gmail.com", userId: 12313, googleToken: 'dfnsjdf',
-    startTrail: () => {
-        return 'checking'
-    },
-    getCoupon: (name: 'coupon200', value: 20) => {
-        return value
-    }
-}
+// interface Admin extends User {
+//     role: "admin" | "ta" | "user",
+// }
+// const Hari: Admin = {
+//     role: "admin",
+//     db_id: 12, email: "hari@gmail.com", userId: 12313, googleToken: 'dfnsjdf',
+//     startTrail: () => {
+//         return 'checking'
+//     },
+//     getCoupon: (name: 'coupon200', value: 20) => {
+//         return value
+//     }
+// }
 
 
 
 // difference between interface and type 
+
+
+class User {
+    public email: string
+    private name: string
+    readonly city: string = "Jaipur"
+    constructor(email: string, name: string) {
+        this.email = email
+        this.name = name
+    }
+}
+
+const a = new User("test@gmail.com", "test")
+a.name
+
 export { }
