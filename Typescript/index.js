df7bc57d5f43d3e9cf360f112c679fd0bb4396a3
Sarthak <sarthakvkumar@gmail.com>
2024-06-03 09:40:21 +0530

    getter and setter in class

--- Typescript/index.js
+++ Typescript/index.js
@@ -1,9 +1,90 @@
 "use strict";
+// type User = {
+//     readonly _id: string;
+//     name: string;
+//     email: string;
+//     isACctive: boolean;
+//     creaditCardDetails?: number
+// }
 Object.defineProperty(exports, "__esModule", { value: true });
-var myUser = {
-    _id: "1242",
-    name: "dsnksjf",
-    email: "kdfnsfn",
-    isACctive: false
-};
-myUser.email = "sarthak@gmail.com";
+// let myUser: User = {
+//     _id: "1242",
+//     name: "dsnksjf",
+//     email: "kdfnsfn",
+//     isACctive: false
+// }
+// myUser.email = "sarthak@gmail.com"
+// Interfaces 
+// interface User {
+//     readonly db_id: number
+//     email: string,
+//     userId: number,
+//     googleId?: string,
+//     startTrail(): string,
+//     getCoupon(couponName: string, value: number): number
+// }
+// interface User {
+//     googleToken: string
+// }
+// interface Admin extends User {
+//     role: "admin" | "ta" | "user",
+// }
+// const Hari: Admin = {
+//     role: "admin",
+//     db_id: 12, email: "hari@gmail.com", userId: 12313, googleToken: 'dfnsjdf',
+//     startTrail: () => {
+//         return 'checking'
+//     },
+//     getCoupon: (name: 'coupon200', value: 20) => {
+//         return value
+//     }
+// }
+// difference between interface and type 
+// class User {
+//     public email: string
+//     private name: string
+//     readonly city: string = "Jaipur"
+//     constructor(email: string, name: string) {
+//         this.email = email
+//         this.name = name
+//     }
+// }
+// const a = new User("test@gmail.com", "test")
+// a.name
+//Getter and Setter in Typescript
+var User = /** @class */ (function () {
+    function User(email, name) {
+        this.email = email;
+        this.name = name;
+        this._courseCount = 1;
+        this.city = "Lucknow";
+    }
+    User.prototype.deleteToken = function () {
+        console.log("Token");
+    };
+    Object.defineProperty(User.prototype, "getAppleEmail", {
+        get: function () {
+            return "apple".concat(this.email);
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(User.prototype, "courseCount", {
+        get: function () {
+            return this._courseCount;
+        },
+        set: function (courseNum) {
+            if (courseNum <= 1) {
+                throw new Error("Course count should be greater than 1");
+            }
+            this._courseCount = courseNum;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    return User;
+}());
+var b = new User("test@hemail.com", "test");
+console.log(b.courseCount);
+b.courseCount = 5;
+console.log(b.courseCount);
--- Typescript/index.ts
+++ Typescript/index.ts
@@ -49,17 +49,54 @@
 // difference between interface and type 
 
 
+// class User {
+//     public email: string
+//     private name: string
+//     readonly city: string = "Jaipur"
+//     constructor(email: string, name: string) {
+//         this.email = email
+//         this.name = name
+//     }
+// }
+
+// const a = new User("test@gmail.com", "test")
+// a.name
+
+//Getter and Setter in Typescript
+
 class User {
-    public email: string
-    private name: string
-    readonly city: string = "Jaipur"
-    constructor(email: string, name: string) {
-        this.email = email
-        this.name = name
+    private _courseCount = 1
+    readonly city: string = "Lucknow"
+    constructor(
+        public email: string,
+        public name: string
+    ) {
+    }
+
+    private deleteToken() {
+        console.log("Token")
+    }
+
+    get getAppleEmail(): string {
+        return `apple${this.email}`
+    }
+
+    get courseCount(): number {
+        return this._courseCount
+    }
+
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("Course count should be greater than 1")
+        }
+
+        this._courseCount = courseNum;
     }
 }
 
-const a = new User("test@gmail.com", "test")
-a.name
+const b = new User("test@hemail.com", "test")
+console.log(b.courseCount)
+b.courseCount = 5
+console.log(b.courseCount)
 
 export { }
