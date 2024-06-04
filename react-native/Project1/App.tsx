bcbc1988b103a7a48bda7db1974bbea174c107f8
Sarthak <sarthakvkumar@gmail.com>
2024-06-04 12:42:20 +0530

    added ActionCard.tsx file

--- react-native/Project1/App.tsx
+++ react-native/Project1/App.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import FlatCard from './components/FlatCard';
 import ElevatedCards from './components/ElevatedCards';
 import FancyCard from './components/FancyCard';
+import ActionCard from './components/ActionCard';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <FlatCard />
         <ElevatedCards />
         <FancyCard />
+        <ActionCard />
       </ScrollView>
     </SafeAreaView>
   );
